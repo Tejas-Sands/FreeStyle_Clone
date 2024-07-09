5e3148753da0afb0b51507a89c6538f41295c1f8
Tejas-Sands <tsandwar@gmail.com>
2024-07-09 16:09:32 +0530

    some final changes III

--- src/components/setups.tsx
+++ src/components/setups.tsx
@@ -134,7 +134,8 @@ export default function Slider(){
                         const match = /language-(\w+)/.exec(className || '');
                         return match ? (
                           <SyntaxHighlighter
-                            style={atomDark}
+                            style={atomDark as any
+                            }
                             language={match[1]}
                             PreTag="pre"
                             className="rounded-md overflow-x-auto max-w-4xl text-[0.6rem] sm:text-sm md:text-base lg:text-base xl:text-base w-full max-h-full p-5"
